9e2338a86a2dc3e80f4d969135d949f069df26a8
Itxaka <itxaka@kairos.io>
2024-11-13 11:14:29 +0100

    Drop unused frameworks (#58)
    * Drop unused frameworks
    
    Signed-off-by: Itxaka <itxaka@kairos.io>
    
    * Add a cutoff to the update script
    
    Signed-off-by: Itxaka <itxaka@kairos.io>
    
    ---------
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- update.sh
+++ update.sh
@@ -9,12 +9,18 @@ PREFIX="v"
 OUTPUT_FILE=images.json
 PAGE=1
 PER_PAGE=100
+CUTOFF_VERSION="v2.10.0"
 
 # Ensure jq_script is properly defined as a single-line string
 jq_script='[.[] | select(.tag_name) | {tag_name: .tag_name, version: .tag_name[1:], image: ("'"$IMAGE_BASE"':" + .tag_name + "'"$SUFFIX"'")}] | group_by(.version | split(".") | .[:2] | join(".")) | map(sort_by(.version | split(".") | map(tonumber)) | reverse | .[0] | {image: .image, tag_name: .tag_name})'
 
 # Initialize or clear the JSON array in the file
 echo "[]" > "$OUTPUT_FILE"
+
+# Function to compare versions
+version_gt() {
+    [ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" ]
+}
 
 # Function to fetch releases, process, and append to the file
 fetch_and_append_releases() {
@@ -28,13 +34,15 @@ fetch_and_append_releases() {
     fi
 
     # Process releases and append to the output file
-    if [ "$ALL_RELEASES" = "true" ]; then
-        local new_data=$(echo "$releases" | jq "[.[] | {image: (\"$IMAGE_BASE:$PREFIX\" + .tag_name + \"$SUFFIX\")}]")
-    else
-        local new_data=$(echo "$releases" | jq "$jq_script")
-    fi
+    local new_data=$(echo "$releases" | jq "$jq_script")
+    local filtered_data="[]"
+    for release in $(echo "$new_data" | jq -r '.[].tag_name'); do
+        if version_gt "$release" "$CUTOFF_VERSION"; then
+            filtered_data=$(echo "$filtered_data" | jq --argjson release "$(echo "$new_data" | jq --arg tag_name "$release" '.[] | select(.tag_name == $tag_name)')" '. + [$release]')
+        fi
+    done
     local existing_data=$(cat "$OUTPUT_FILE")
-    echo "$existing_data" "$new_data" | jq -s '.[0] + .[1]' > "$OUTPUT_FILE.tmp" && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
+    echo "$existing_data" "$filtered_data" | jq -s '.[0] + .[1] | group_by(.tag_name | split(".")[:2] | join(".")) | map(max_by(.tag_name))' > "$OUTPUT_FILE.tmp" && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
 
     return 0
 }
